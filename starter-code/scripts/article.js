3ea44f81fa3ad94a8a29293a427254cd4af2a92f
Miya Lucas <srlucas3@gmail.com>
2017-08-06 18:25:27 -0700

    finished lab 4

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -16,6 +16,19 @@ Article.prototype.toHtml = function() {
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
+  var source = $('#1st-handlebars-template').html();
+  var templates = Handlebars.compile(source);
+
+  Article.forEach(function(article) {
+      $('#articles').append(templates(article));
+  });
+
+  ////we grab the script tag and put .html on it
+  ////call handlebars.compile
+  ///this creates the html string
+  ////we then pass that into the function to append to the element with ID of articles
+  ////call compile function
+
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -24,7 +37,7 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+////call template function
 };
 
 rawData.sort(function(a,b) {
